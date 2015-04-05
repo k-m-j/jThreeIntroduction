556655d03f261e52a2958ebeeb3b7e5809624315
atari <atari@atari-no-MacBook-Pro.local>
2015-04-05 15:36:51 +0900

    plugin等を読み込んだ状態にした。

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ jThree( function( j3 ) {
 	j3.Trackball();
 	j3.Stats();
 
+
+/*
 	j3( "mesh" ).click( function() {
 
 		var mode = + ( j3( this ).attr( "geo" ) == "#geo0" );
@@ -12,6 +14,7 @@ jThree( function( j3 ) {
 		.animate( { mtlColor: mode ? "#f00" : "#00f" }, 500 );
 
 	} );
+*/
 
 },
 function() {
