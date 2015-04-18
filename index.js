df63022d1994b7eb3518ea78abf1649cb29ea5d9
atari <atari@atari-no-MacBook-Pro.local>
2015-04-18 20:49:31 +0900

    プログレスバーの表示

--- index.js
+++ index.js
@@ -4,17 +4,7 @@ jThree( function( j3 ) {
 
 	j3.Trackball();
 	j3.Stats();
-
-
-/*
-	j3( "mesh" ).click( function() {
-
-		var mode = + ( j3( this ).attr( "geo" ) == "#geo0" );
-		j3( this ).attr( "geo", "#geo" + mode )
-		.animate( { mtlColor: mode ? "#f00" : "#00f" }, 500 );
-
-	} );
-*/
+	jThree.MMD.sync('#audio');
 
 },
 function() {
