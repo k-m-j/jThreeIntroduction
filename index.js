61bd4a44fbc320edf2dc5cb76f1c030e65862711
atari <atari@atari-no-MacBook-Pro.local>
2015-05-17 17:36:13 +0900

    add authorized stageData, delete unAuthorized data, mmd loader v1.6

--- index.js
+++ index.js
@@ -5,21 +5,9 @@ jThree( function( j3 ) {
 	j3.Trackball();
 	j3.Stats();
 
-
-/*
-	j3( "mesh" ).click( function() {
-
-		var mode = + ( j3( this ).attr( "geo" ) == "#geo0" );
-		j3( this ).attr( "geo", "#geo" + mode )
-		.animate( { mtlColor: mode ? "#f00" : "#00f" }, 500 );
-
-	} );
-*/
-
 	$("#btn").click( function() {
 		j3("#cube").attr( "mtl", "#mtlBlue");
 	} );
-
 
 },
 function() {
