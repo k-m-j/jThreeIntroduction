7e629a42f65d2d2df5897638ada1318d007b9fdb
atari <atari@atari-no-MacBook-Pro.local>
2015-05-20 14:06:19 +0900

    add motion mmd, move obj

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ jThree( function( j3 ) {
 	
 	j3.Trackball();
 	j3.Stats();
+	jThree.MMD.play();
+
+	jThree(function ( j3 ) {
+        j3( "#cube" ).animate( { positionX: "-=50", positionY: "-=50"}, 20000);
+    });
 
 },
 function() {
