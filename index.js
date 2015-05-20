cffa5fba6cff0cf42ba9758a9d6a576ea72cd9e2
atari <atari@atari-no-MacBook-Pro.local>
2015-05-20 22:58:15 +0900

    rotate earth

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ jThree( function( j3 ) {
 	j3.Trackball();
 	j3.Stats();
 
+	function rotateEarth() {
+		j3("#earth").animate({ rotateY: "+=3.14" }, 5000, rotateEarth);
+	}
+	rotateEarth();
+
 },
 function() {
 	alert( "このブラウザはWebGLに対応していません。" );
