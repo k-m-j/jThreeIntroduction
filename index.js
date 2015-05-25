bee1592b1926ee0a4e66dcbe16a81340364f36be
atari <atari@atari-no-MacBook-Pro.local>
2015-05-25 23:37:01 +0900

    テキストテクスチャの動的変更

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ jThree( function( j3 ) {
 	j3.Trackball();
 	j3.Stats();
 
+	$("#btn").click(function(){
+		var text = $("#text").val();
+
+		j3("import").contents().find('#text').html(text);
+		j3('#txr').attr('html','#text');
+	});
 },
 function() {
 	alert( "このブラウザはWebGLに対応していません。" );
