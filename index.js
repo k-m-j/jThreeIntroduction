f876a1e8efd8a18a31feedb6f99c78b68425dfa2
atari <atari@atari-no-MacBook-Pro.local>
2015-05-25 23:10:15 +0900

    オブジェクトの動的追加

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ jThree( function( j3 ) {
 	j3.Trackball();
 	j3.Stats();
 
+	$("#btn").click(function (){
+		var　positionX = parseInt(Math.random()*200)-100,
+			 meshTag = '<mesh class="cube" geo="#geo" mtl="#mtl" style="positionX: ' + positionX +'; positionZ: -100;"></mesh>';
+
+		j3("scene").prepend(meshTag);
+	});
 },
 function() {
 	alert( "このブラウザはWebGLに対応していません。" );
